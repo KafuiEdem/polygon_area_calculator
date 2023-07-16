a5c7a938ef177647b857d33e23cee1491e37947a
reamedzo@st.ug.edu.gh <reamedzo@st.ug.edu.gh>
2023-07-16 04:53:20 +0000

    Updating project

--- shape_calculator.py
+++ shape_calculator.py
@@ -56,8 +56,46 @@ class Rectangle:
         result = f"Rectangel(width={self.width},height={self.height})"
         return result
 
+#setting the squre class
 class Square(Rectangle):
-    pass
+    #defining the contstructor
+    def __init__(self,length, width=0, height=0) -> None:
+        super().__init__(width, height)
+        self.length = length
+        self.side = 0
+    
+    #get_area method from Rectangle
+    def get_area(self):
+        return self.length **2
+    
+    #setting the set_side method
+    def set_side(self,side):
+        self.side += side
+    
+    #setting the idagonal
+    def get_diagonal(self):
+        diagonal = ((self.length ** 2 + self.length ** 2) ** 0.5)
+        return diagonal
+    
+    def __str__(self) -> str:
+        result = f"Square(side={self.length})"
+        return result
+    
+    #getting the picture
+    def get_picture(self):
+        length = "*" 
+        side = self.side
+        breath = side
+        
+        while breath !=0:
+            if self.height > 50 or self.width >50:
+                print("Too big for picture.")
+                break
+            else:
+                c = length * side
+                breath -=1
+                print(c)
+    
 
 c = Rectangle(10,5)
 print(c.get_area())
@@ -65,3 +103,10 @@ c.set_height(3)
 print(c.get_perimeter())
 print(c)
 c.get_picture()
+
+s = Square(9)
+print(s.get_area())
+print(s.get_diagonal())
+print(s)
+s.set_side(4)
+s.get_picture()
