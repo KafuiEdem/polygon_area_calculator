648f1bc05e4c86aea5b446acd1f943f28f6cd841
reamedzo@st.ug.edu.gh <reamedzo@st.ug.edu.gh>
2023-07-17 00:24:24 +0000

    Updating

--- shape_calculator.py
+++ shape_calculator.py
@@ -106,23 +106,25 @@ class Square(Rectangle):
                 breath -=1
                 print(c)
                 
-                
+if __name__ == "__main__":            
     
 
-rect = Rectangle(10, 5)
-print(rect.get_area())
-rect.set_height(3)
-print(rect.get_perimeter())
-print(rect)
-rect.get_picture()
+    rect = Rectangle(10, 5)
+    print(rect.get_area())
+    rect.set_height(3)
+    print(rect.get_perimeter())
+    print(rect)
+    rect.get_picture()
 
-sq = Square(9)
-print(sq.get_area())
-sq.set_side(4)
-print(sq.get_diagonal())
-print(sq)
-print(sq.get_picture())
+    sq = Square(9)
+    print(sq.get_area())
+    sq.set_side(4)
+    print(sq.get_diagonal())
+    print(sq)
+    print(sq.get_picture())
 
-rect.set_height(8)
-rect.set_width(16)
-print(rect.get_amount_inside(sq))
+    rect.set_height(8)
+    rect.set_width(16)
+    print(rect.get_amount_inside(sq))
+
+
